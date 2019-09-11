c7fb8f00972eec45d8d0cd8a3ca77927888ea78e
Pecaalta <pecaalta@gmail.com>
2019-09-11 19:30:15 -0300

    Se sube a git

---
+++ BusinessLogicLayer/BLEmployees.cs
@@ -0,0 +1,73 @@
+﻿using DataAccessLayer;
+using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class BLEmployees : IBLEmployees
+    {
+       private IDALEmployees _dal;
+
+        public BLEmployees(IDALEmployees dal)
+        {
+            _dal = dal;
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            DALEmployeesEF mEmp = new DALEmployeesEF();
+            mEmp.AddEmployee(emp);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            DALEmployeesEF emp = new DALEmployeesEF();
+            emp.DeleteEmployee(id);
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            DALEmployeesEF emp = new DALEmployeesEF();
+            return emp.UpdateEmployee(emp);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            DALEmployeesEF emp = new .DALEmployeesEF();
+            return emp.GetAllEmployees();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            DALEmployeesEF emp = new DALEmployeesEF();
+            if (emp == null)
+            {
+                throw new Exception("El usuario no se encontro");
+            }
+            return emp.GetEmployee(id);
+        }
+
+        public double CalcPartTimeEmployeeSalary(int idEmployee, int hours)
+        {
+            DALEmployeesEF emp = new DALEmployeesEF();
+            if (emp == null)
+            {
+                throw new Exception("El usuario no se encontro");
+            }
+            Employee mEmp = emp.GetEmployee(idEmployee);
+            if (mEmp.GetType().Name == "FullTimeEmployee")
+            {
+                throw new Exception("El usuario no es part-time");
+            }
+            else
+            {
+                return ((PartTimeEmployee)mEmp).HourlyRate * hours;
+            }
+
+        }
+    }
+}
---
+++ DataAccessLayer/DALEmployeesEF.cs
@@ -0,0 +1,146 @@
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DALEmployeesEF : IDALEmployees
+    {
+        private Model.EmployeesTPH cast(Employee emp)
+        {
+            if (emp == null)
+            {
+                return null;
+            }
+            if (emp.GetType().Name == "FullTimeEmployee")
+            {
+                FullTimeEmployee employee = (FullTimeEmployee)emp;
+                Model.FullTimeEmployees employeeBase = new Model.FullTimeEmployees()
+                {
+                    name = employee.Name,
+                    StartDate = employee.StartDate,
+                    Salary = employee.Salary
+                };
+                return employeeBase;
+            }
+            else
+            {
+                PartTimeEmployee employee = (PartTimeEmployee)emp;
+                Model.PartTimeEmployees employeeBase = new Model.PartTimeEmployees()
+                {
+                    name = employee.Name,
+                    StartDate = employee.StartDate,
+                    HourlyRate = employee.HourlyRate
+                };
+                return employeeBase;
+            }
+
+        }
+
+        private Employee cast(Model.EmployeesTPH emp)
+        {
+            if (emp == null)
+            {
+                return null;
+            }
+            if (emp.GetType().Name == "FullTimeEmployee")
+            {
+                Model.FullTimeEmployees employee = (Model.FullTimeEmployees)emp;
+                FullTimeEmployee employeeBase = new FullTimeEmployee()
+                {
+                    Id = employee.EmployeeId,
+                    Name = employee.name,
+                    StartDate = employee.StartDate,
+                    Salary = employee.Salary
+                };
+                return employeeBase;
+            }
+            else
+            {
+                Model.PartTimeEmployees employee = (Model.PartTimeEmployees)x;
+                PartTimeEmployee employeeBase = new PartTimeEmployee()
+                {
+                    Id = employee.EmployeeId,
+                    Name = employee.name,
+                    StartDate = employee.StartDate,
+                    HourlyRate = employee.HourlyRate
+                };
+                return employeeBase;
+            }
+
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            if (emp == null)
+            {
+                return;
+            }
+            using (Model.Obligatorio1Entities en = new Model.Obligatorio1Entities())
+            {
+                en.EmployeesTPH.Add(cast(emp));
+                en.SaveChanges();
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            using (Model.Obligatorio1Entities en = new Model.Obligatorio1Entities())
+            {
+                en.EmployeesTPH.Remove(
+                    en.EmployeesTPH.Find(id)
+                );
+                en.SaveChanges();
+            }
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            if (emp == null)
+            {
+                return;
+            }
+            using (Model.Obligatorio1Entities en = new Model.Obligatorio1Entities())
+            {
+                Model.EmployeesTPH em = en.EmployeesTPH.Find(emp.Id);
+                em.name = emp.Name;
+                em.StartDate = emp.StartDate;
+                if (emp.GetType().Name == "FullTimeEmployee")
+                {
+                    Model.FullTimeEmployees emCast = (Model.FullTimeEmployees)em;
+                    emCast.Salary = ((FullTimeEmployee)emp).Salary;
+                }
+                else
+                {
+                    Model.PartTimeEmployees emCast = (Model.PartTimeEmployees)em;
+                    emCast.HourlyRate = ((PartTimeEmployee)emp).HourlyRate;
+                }
+                en.SaveChanges();
+            }
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            using (Model.Obligatorio1Entities en = new Model.Obligatorio1Entities())
+            {
+                List<Employee> listaEmpleados = new List<Employee>();
+                en.EmployeesTPH.ToList().ForEach(x =>
+                {
+                    listaEmpleados.Add(cast(x));
+                });
+                return listaEmpleados;
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            using (Model.Obligatorio1Entities en = new Model.Obligatorio1Entities())
+            {
+                return cast(en.EmployeesTPH.Find(id));
+            }
+        }
+    }
+}
